bd558a4df93a26c81b0b304717f34ca39a4763bd
zhangzuoge <fengya011@163.com>
2016-11-25 18:13:06 +0800

    WNDRegist
    WNDBindPhone
    WNDSetPassword
    WNDVerifyPhone

--- project/Assets/Scripts/Editor/ww.cs
+++ project/Assets/Scripts/Editor/ww.cs
@@ -4,7 +4,62 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class ww  {
-    [MenuItem("test/te")]
+    static  string titleName="newTitle/";
+
+
+    //[ExecuteInEditMode]
+    [MenuItem("newTitle/EditorStart  %h")]
+    static void EditorStart()
+    {
+        //EditorApplication.update = EditorUpdate;
+        EditorUpdate();
+    }
+    [MenuItem("newTitle/GameObjectHide %g")]
+    static void GameObjectHide()
+    {
+        Transform obj = Selection.activeTransform;
+        if (obj.name != null)
+        {
+
+            obj.gameObject.SetActive(!obj.gameObject.activeSelf);
+
+        }
+        else
+        {
+        }
+
+    }
+    static void EditorUpdate()
+    {
+        Transform[] objs = Selection.GetTransforms(SelectionMode.Unfiltered);
+        foreach (Transform obj in objs)
+        {
+            if (obj.name == "UIROOT")
+            {
+
+                    obj.gameObject.SetActive(!obj.gameObject.activeSelf);
+
+            }
+            else
+            {
+            }
+        }
+        //if (EditorApplication.isPlaying)
+        //{
+        //    Debug.Log("sfdf");
+
+        //}
+        //else
+        //{
+        //}
+    }
+    [MenuItem("newTitle/EditorFinish")]
+    static void EditorFinish()
+    {
+        EditorApplication.update = null;
+
+    }
+    [MenuItem("newTitle/resources")]
     public static void te()
     {
         Debug.Log("start");
@@ -19,6 +74,46 @@ public class ww  {
             Debug.Log(assets);
         }
 
+    }
+    [MenuItem("newTitle/textFun")]
+    public static void textFun()
+    {
+        Debug.Log("Path"+Application.dataPath);
+        Debug.Log("Path" + Application.persistentDataPath);
+        Debug.Log("Path" + Application.streamingAssetsPath);
+        Debug.Log("Path" + Application.temporaryCachePath);
+    }
+
+    [MenuItem("newTitle/GameObjectList")]
+    static void GameObjectList()
+    {
+        EditorApplication.update = EditorUpdate;
+
+    }
+    static void GameObjectListUpdate()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            Debug.Log("sfdf");
+
+        }
+        else
+        {
+            Transform[] objs = Selection.GetTransforms(SelectionMode.Unfiltered);
+            foreach (Transform obj in objs)
+            {
+                if (obj.name == "UIROOT")
+                {
+                    if (obj.gameObject.activeSelf)
+                    {
+                        obj.gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                }
+            }
+        }
     }
 
 }
