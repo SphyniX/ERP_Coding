b0ab24c8fc4e1a5dabe1f9198e24ad5bc1171930
zhangzuoge <fengya011@163.com>
2016-12-01 13:41:24 +0800

    调整界面，添加督导巡店记录

---
+++ project/Essets/LuaRoot/ui/attendance/lc_wndsuppunchtabb.lua
@@ -0,0 +1,62 @@
+--
+-- @file    ui/attendance/lc_wndsuppunchtabb.lua
+-- @authors cks
+-- @date    2016-12-01 08:38:24
+-- @desc    WNDSupPunchTabb
+--
+
+local ipairs, pairs
+    = ipairs, pairs
+local libugui = require "libugui.cs"
+local libunity = require "libunity.cs"
+local UIMGR = MERequire "ui/uimgr"
+local Ref
+--!*以下：自动生成的回调函数*--
+
+local function on_subtop_submouth_btnnew_click(btn)
+	
+end
+
+local function on_subtop_submouth_btnlast_click(btn)
+	
+end
+
+local function on_subtop_btnback_click(btn)
+	
+end
+
+local function init_view()
+	Ref.SubTop.SubMouth.btnNew.onAction = on_subtop_submouth_btnnew_click
+	Ref.SubTop.SubMouth.btnLast.onAction = on_subtop_submouth_btnlast_click
+	Ref.SubTop.btnBack.onAction = on_subtop_btnback_click
+	UIMGR.make_group(Ref.SubLog.GrpLog)
+	--!*以上：自动注册的回调函数*--
+end
+
+local function init_logic()
+	
+end
+
+local function start(self)
+	if Ref == nil or Ref.root ~= self then
+		Ref = libugui.GenLuaTable(self, "root")
+		init_view()
+	end
+	init_logic()
+end
+
+local function update_view()
+	
+end
+
+local function on_recycle()
+	
+end
+
+local P = {
+	start = start,
+	update_view = update_view,
+	on_recycle = on_recycle,
+}
+return P
+
